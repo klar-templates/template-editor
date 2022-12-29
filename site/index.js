5bfbd810975895075ad53909d9aa3374360dab0d
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-30 00:17:16 +0100

    wip

--- site/index.js
+++ site/index.js
@@ -18,7 +18,7 @@ const initSite = function () {
   window.templateComponents = parent.frames.templateComponents;
   // Add klar-pages-app script after Babel has transpiled the JSX code
   const script = document.createElement('script');
-  script.src = 'http://localhost:4173/assets/index.4c81e3cb.js';
+  script.src = 'http://localhost:4173/assets/index.717d46a8.js';
   script.type = 'module';
   script.crossOrigin = true;
   document.querySelector('head').appendChild(script);
